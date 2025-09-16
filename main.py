f2477c05c1409677f2dbaec451dd5b6f6cb9741e
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2025-09-15 20:26:38 -0500

    Added camelcase function and accompanying code in main to call function

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+def camelcase(sentence):
+    """Convert sentence to camelCase"""
+    title_case = sentence.title() #uppercase first letter ea. word
+    upper_camel_cased = title_case.replace(' ', '')
+    return upper_camel_cased[0:1].lower() + upper_camel_cased[1:]
+
 def banner():
     """Display program name"""
     message = 'The wonderful camelCase program!!!!!'
@@ -6,5 +12,9 @@ def banner():
 
 def main():
     banner()
+    sentence = input('Enter your sentence ')
+    output = camelcase(sentence)
+    print(output)
 
-main()
+if __name__ == '__main__':
+    main()
